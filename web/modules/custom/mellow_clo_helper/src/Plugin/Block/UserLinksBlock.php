14705283ea297e15a4951ff4b3c8adfe2a400312
eliooooooo <eliott.burkle@gmail.com>
2025-01-24 10:01:15 +0100

    Feat: Mellow Clo Helper module

---
+++ web/modules/custom/mellow_clo_helper/src/Plugin/Block/UserLinksBlock.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\mellow_clo_helper\Plugin\Block;
+
+use Drupal;
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Block\Attribute\Block;
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+/**
+ * Provides an user links block.
+ */
+#[Block(
+  id: 'mellow_clo_helper_user_links',
+  admin_label: new TranslatableMarkup('User links'),
+  category: new TranslatableMarkup('Custom'),
+)]
+final class UserLinksBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  public function __construct(
+    private readonly EntityTypeManagerInterface $entityTypeManager,
+    private readonly AccountInterface $currentUser,
+    private readonly UrlGeneratorInterface $urlGeneratorInterface,
+    ) {}
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): UserLinksBlock {
+    return new UserLinksBlock(
+        $container->get('entity_type.manager'),
+        $container->get('current_user'),
+        $container->get('url_generator')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(): array {
+    $build = [];
+
+    $cartCount = $this->entityTypeManager->getStorage('node')->loadByProperties(
+      ['type' => 'cart', 'field_cart_user' => $this->currentUser->id()]
+    );
+    $cartCount = array_pop($cartCount);
+
+    $build['user_links_block'] = [
+      '#theme' => 'mellow_clo_helper_user_links_block_theme',
+      '#user_cart' => [
+        'url' => $this->urlGeneratorInterface->generate('mellow_clo_manager.cart.show'),
+        'count' => $cartCount ? $cartCount->get('field_cart_products')->count() : 0,
+      ],
+      '#user_account' => [
+        'url' => $this->urlGeneratorInterface->generate('mellow_clo_manager.account.show'),
+      ],
+    ];
+
+    return $build;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function blockAccess(AccountInterface $account): AccessResult {
+    // @todo Evaluate the access condition here.
+    return AccessResult::allowedIf(TRUE);
+  }
+
+}
