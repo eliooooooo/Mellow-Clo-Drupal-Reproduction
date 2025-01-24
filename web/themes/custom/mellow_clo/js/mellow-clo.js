d29c2c81e06e8c01c220199deba0fca0801aa660
eliooooooo <eliott.burkle@gmail.com>
2025-01-24 09:20:28 +0100

    Chores: Custom theme init

---
+++ web/themes/custom/mellow_clo/js/mellow-clo.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * mellow_clo behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.mellowClo = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
